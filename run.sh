082739bff781f0b402225bdaaeee3d8b377b04ba
Fernando Correia <fernando@servicero.com>
2014-01-10 17:06:35 -0200

    Convert tab to spaces

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@
 set -o nounset -o errexit
 
 test() {
-	local test_name=$1
+  local test_name=$1
   local result_file="results/$test_name.txt"
   echo "tail -f $result_file for progress"
   "tests/test-sbt-incremental-$test_name.sh" > $result_file
